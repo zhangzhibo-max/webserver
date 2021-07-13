983fd73faf24ec6f7571ac96ac5825e1c237789f
张智博 <764215962@qq.com>
2021-07-13 14:45:43 +0800

    update

--- public.cpp
+++ public.cpp
@@ -22,7 +22,7 @@ char* string_to_gbk(const char* chUTF)
 std::string string_To_UTF8(const std::string& str)
 {
 	int nwLen = ::MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
-	wchar_t* pwBuf = new wchar_t[nwLen + 1];//一定要加1，不然会出现尾巴 
+	wchar_t* pwBuf = new wchar_t[nwLen + 1]; 
 	ZeroMemory(pwBuf, nwLen * 2 + 2);
 	::MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), pwBuf, nwLen);
 	int nLen = ::WideCharToMultiByte(CP_UTF8, 0, pwBuf, -1, NULL, NULL, NULL, NULL);
